1e04c8bb4cc4d12d8abe7491c149587c15bf2c76
Mikhail Zainutdinov <michaelzainutdinov@ya.ru>
2025-06-18 21:17:37 +0300

    User service

---
+++ internal/service/user_service.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/a2sh3r/gophermart/internal/apperrors"
+
+	"github.com/a2sh3r/gophermart/internal/models"
+	"github.com/a2sh3r/gophermart/internal/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService interface {
+	Register(ctx context.Context, login, password string) error
+	Authenticate(ctx context.Context, login, password string) error
+}
+
+type userService struct {
+	repo repository.UserRepository
+}
+
+func NewUserService(repo repository.UserRepository) UserService {
+	return &userService{repo: repo}
+}
+
+func (s *userService) Register(ctx context.Context, login, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user := &models.User{
+		Login:    login,
+		Password: string(hashedPassword),
+	}
+
+	err = s.repo.CreateUser(ctx, user)
+	if errors.Is(err, apperrors.ErrUserAlreadyExists) {
+		return err
+	}
+	return err
+}
+
+func (s *userService) Authenticate(ctx context.Context, login, password string) error {
+	user, err := s.repo.GetUserByLogin(ctx, login)
+	if err != nil {
+		return apperrors.ErrInvalidCredentials
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return apperrors.ErrInvalidCredentials
+	}
+
+	return nil
+}
